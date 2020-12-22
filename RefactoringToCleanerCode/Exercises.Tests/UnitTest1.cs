09ea9c192d2a4abe46c3370a3943229e4721031a
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2020-12-22 19:03:23 +0000

    Empty project

---
+++ RefactoringToCleanerCode/Exercises.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new Class1()
+        }
+    }
+}
