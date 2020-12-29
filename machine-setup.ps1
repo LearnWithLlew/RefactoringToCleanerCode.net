da6ae6fbfa6541fb5907f7cde1f4beb906fcfba9
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2020-12-29 16:10:14 +0000

    e   Add machine setup

---
+++ machine-setup.ps1
@@ -0,0 +1,10 @@
+# To run this script directly, open PowerShell and run this command:
+#     PS> Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/RefactoringToCleanerCode.net/master/machine-setup.ps1 | Invoke-Expression
+
+#Requires -RunAsAdministrator
+
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/visual-studio.ps1 | iex
+
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/RefactoringToCleanerCode.net.git C:\Source\RefactoringToCleanerCode.net
+github C:\Source\RefactoringToCleanerCode.net
