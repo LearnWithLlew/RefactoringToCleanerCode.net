5ddd22a29b08d71244022b40f1e42fb6d718283a
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2020-12-29 18:14:51 +0000

    add fonts to machine setup

--- machine-setup.ps1
+++ machine-setup.ps1
@@ -10,3 +10,9 @@ cinst typora
 
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/RefactoringToCleanerCode.net.git C:\Source\RefactoringToCleanerCode.net
 github C:\Source\RefactoringToCleanerCode.net
+
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/RefactoringToCleanerCode.slides.git C:\Source\RefactoringToCleanerCode.slides
+
+Expand-Archive C:\Source\RefactoringToCleanerCode.Slides\fonts\playfair-display.zip C:\Source\RefactoringToCleanerCode.Slides\fonts\
+Expand-Archive C:\Source\RefactoringToCleanerCode.Slides\fonts\pt-serif.zip
+Invoke-Item C:\Source\RefactoringToCleanerCode.Slides\fonts\
