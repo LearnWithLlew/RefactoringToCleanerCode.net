14382eb6fe977d89e73b871bd6ed30b7b07f28e0
Jay Bazuzi <jay@bazuzi.com>
2021-01-03 20:41:58 +0000

    Extract utilities

--- RefactoringToCleanerCode/Exercises.Tests/PinningTest.cs
+++ RefactoringToCleanerCode/Exercises.Tests/PinningTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using ApprovalTests.Combinations;
 using ApprovalTests.Reporters;
@@ -14,10 +15,24 @@ namespace Exercises.Tests
         public void PinEverything()
         {
             var dice = new[] {1, 2, 3, 4, 5, 6};
-            var scoringTypes = Enum.GetValues(typeof(ScoringType)).Cast<ScoringType>().Append((ScoringType) (-1));
-            CombinationApprovals.VerifyAllCombinations(
-                (a, b, c, d, e, f) => BigDiceGame.GetScore(a, b, c, d, e, f),
-                scoringTypes, dice, dice, dice, dice, dice);
+            var scoringTypes = Utilities.GetEnumValues<ScoringType>().Append((ScoringType) (-1));
+            Utilities.VerifyAllCombinations(BigDiceGame.GetScore, scoringTypes, dice, dice, dice, dice, dice);
         }
+    }
+}
+
+internal static class Utilities
+{
+    public static IEnumerable<TEnum> GetEnumValues<TEnum>()
+    {
+        return (TEnum[]) Enum.GetValues(typeof(TEnum));
+    }
+
+    public static void VerifyAllCombinations<A, B, C, D, E, F, Result>(Func<A, B, C, D, E, F, Result> processCall,
+        IEnumerable<A> aList, IEnumerable<B> bList, IEnumerable<C> cList, IEnumerable<D> dList, IEnumerable<E> eList,
+        IEnumerable<F> fList)
+    {
+        CombinationApprovals.VerifyAllCombinations((a, b, c, d, e, f) => processCall(a, b, c, d, e, f), aList, bList,
+            cList, dList, eList, fList);
     }
 }
