a26d7f64996fcf4674d581bd2be390dee0b46469
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:23:59 +0000

    r - surround with block

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,9 +2,11 @@
 {
     public static int GetScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
-        if (new Pair().Matches(scoringType))
         {
-            return new Pair().GetScore(die1, die2, die3, die4, die5);
+            if (new Pair().Matches(scoringType))
+            {
+                return new Pair().GetScore(die1, die2, die3, die4, die5);
+            }
         }
 
         if (new TwoPair().Matches(scoringType))
