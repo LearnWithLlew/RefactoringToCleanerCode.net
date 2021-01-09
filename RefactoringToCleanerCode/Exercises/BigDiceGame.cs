b17ab494be157308dae4369396c61664391e3e4f
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 21:39:30 +0000

    r - extract method for pair case

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -7,23 +7,7 @@
         {
             case ScoringType.Pair:
             {
-                const int value = 2;
-                int[] tallies1;
-                tallies1 = new int[6];
-                tallies1[die1 - 1]++;
-                tallies1[die2 - 1]++;
-                tallies1[die3 - 1]++;
-                tallies1[die4 - 1]++;
-                tallies1[die5 - 1]++;
-                for (var i = 6 - 1; i >= 0; i--)
-                {
-                    if (tallies1[i] >= value)
-                    {
-                        return (i + 1) * value;
-                    }
-                }
-
-                return 0;
+                return Applesauce(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.TwoPair:
@@ -114,6 +98,27 @@
                 }
 
                 return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int Applesauce(int die1, int die2, int die3, int die4, int die5)
+    {
+        const int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[die1 - 1]++;
+        tallies1[die2 - 1]++;
+        tallies1[die3 - 1]++;
+        tallies1[die4 - 1]++;
+        tallies1[die5 - 1]++;
+        for (var i = 6 - 1; i >= 0; i--)
+        {
+            if (tallies1[i] >= value)
+            {
+                return (i + 1) * value;
             }
         }
 
