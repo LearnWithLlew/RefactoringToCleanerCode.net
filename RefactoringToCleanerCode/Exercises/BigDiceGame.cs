78a109844df2128c910dca97df4ff292e6d92a54
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:53:50 +0000

    r - to linq expression

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -1,16 +1,11 @@
-﻿public static class BigDiceGame
+﻿using System.Linq;
+
+public static class BigDiceGame
 {
     public static int GetScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
-        foreach (ICategory category in new ICategory[]
-            {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()})
-        {
-            if (category.Matches(scoringType))
-            {
-                return category.GetScore(die1, die2, die3, die4, die5);
-            }
-        }
-
-        return 0;
+        return (new ICategory[] {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()}
+            .Where(category => category.Matches(scoringType))
+            .Select(category => category.GetScore(die1, die2, die3, die4, die5))).FirstOrDefault();
     }
 }
