912e0033ffea4d85170213a73eaf4f203e9cc197
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:52:44 +0000

    r - rename variable

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,12 +2,12 @@
 {
     public static int GetScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
-        foreach (ICategory appleSauce in new ICategory[]
+        foreach (ICategory category in new ICategory[]
             {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()})
         {
-            if (appleSauce.Matches(scoringType))
+            if (category.Matches(scoringType))
             {
-                return appleSauce.GetScore(die1, die2, die3, die4, die5);
+                return category.GetScore(die1, die2, die3, die4, die5);
             }
         }
 
