9c34b8ca897a8ba1ddb6def06991448706139b19
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:24:49 +0000

    r - surround with block

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -16,9 +16,11 @@
             }
         }
 
-        if (new FullHouse().Matches(scoringType))
         {
-            return new FullHouse().GetScore(die1, die2, die3, die4, die5);
+            if (new FullHouse().Matches(scoringType))
+            {
+                return new FullHouse().GetScore(die1, die2, die3, die4, die5);
+            }
         }
 
         if (new FiveOfAKind().Matches(scoringType))
