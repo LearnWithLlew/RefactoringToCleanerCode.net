f4dece795173cba20ee194c17784cc921fec838c
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 21:55:31 +0000

    r - extract class

---
+++ RefactoringToCleanerCode/Exercises/FullHouse.cs
@@ -0,0 +1,45 @@
+static internal class FullHouse
+{
+    public static int AppleSauce3(int die1, int die2, int die3, int die4, int die5)
+    {
+        int[] tallies;
+        var _2 = false;
+        int i;
+        var _2_at = 0;
+        var _3 = false;
+        var _3_at = 0;
+
+
+        tallies = new int[6];
+        tallies[die1 - 1] += 1;
+        tallies[die2 - 1] += 1;
+        tallies[die3 - 1] += 1;
+        tallies[die4 - 1] += 1;
+        tallies[die5 - 1] += 1;
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 2)
+            {
+                _2 = true;
+                _2_at = i + 1;
+            }
+        }
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 3)
+            {
+                _3 = true;
+                _3_at = i + 1;
+            }
+        }
+
+        if (_2 && _3)
+        {
+            return _2_at * 2 + _3_at * 3;
+        }
+
+        return 0;
+    }
+}
