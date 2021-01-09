4dfd11df74c24791778be38f53b2e60d78a92883
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:49:30 +0000

    R - add twopair

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,7 +3,7 @@
     public static int GetScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            foreach (INonSenseApple appleSauce in new INonSenseApple[]{ new Pair()})
+            foreach (INonSenseApple appleSauce in new INonSenseApple[]{ new Pair(), new TwoPair()})
             {
                 if (appleSauce.Matches(scoringType))
                 {
@@ -12,13 +12,6 @@
             }
         }
 
-        {
-            INonSenseApple appleSauce = new TwoPair();
-            if (appleSauce.Matches(scoringType))
-            {
-                return appleSauce.GetScore(die1, die2, die3, die4, die5);
-            }
-        }
 
         {
             INonSenseApple appleSauce = new FullHouse();
