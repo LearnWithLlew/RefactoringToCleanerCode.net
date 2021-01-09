f27e92128a43c199d67f38a2109e9f89bfe8e708
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:50:27 +0000

    R - add FiveOfAKind

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,20 +3,12 @@
     public static int GetScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            foreach (INonSenseApple appleSauce in new INonSenseApple[]{ new Pair(), new TwoPair(), new FullHouse()})
+            foreach (INonSenseApple appleSauce in new INonSenseApple[]{ new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()})
             {
                 if (appleSauce.Matches(scoringType))
                 {
                     return appleSauce.GetScore(die1, die2, die3, die4, die5);
                 }
-            }
-        }
-
-        {
-            INonSenseApple appleSauce = new FiveOfAKind();
-            if (appleSauce.Matches(scoringType))
-            {
-                return appleSauce.GetScore(die1, die2, die3, die4, die5);
             }
         }
 
