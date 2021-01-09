05927284cee1178b909700b6eb1d2ea3db6b7adc
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 21:59:20 +0000

    r - converted switch to if/else

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,27 +2,21 @@
 {
     public static int GetScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        switch (st)
+        if (st == ScoringType.Pair)
         {
-            case ScoringType.Pair:
-            {
-                return Pair.Applesauce(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.TwoPair:
-            {
-                return TwoPair.AppleSauce2(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.FullHouse:
-            {
-                return FullHouse.AppleSauce3(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.FiveOfAKind:
-            {
-                return FiveOfAKind.AppleSauce4(die1, die2, die3, die4, die5);
-            }
+            return Pair.Applesauce(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.TwoPair)
+        {
+            return TwoPair.AppleSauce2(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.FullHouse)
+        {
+            return FullHouse.AppleSauce3(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.FiveOfAKind)
+        {
+            return FiveOfAKind.AppleSauce4(die1, die2, die3, die4, die5);
         }
 
         return 0;
