88a33f8032f69c838d136160e4d01a400798524f
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 21:51:57 +0000

    r - extract method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -21,23 +21,28 @@
 
             case ScoringType.FiveOfAKind:
             {
-                int[] dice1 = {die1, die2, die3, die4, die5};
-                var counts2 = new int[6];
-                foreach (var die in dice1)
-                {
-                    counts2[die - 1]++;
-                }
+                return AppleSauce4(die1, die2, die3, die4, die5);
+            }
+        }
 
-                for (var i1 = 0; i1 != 6; i1++)
-                {
-                    if (counts2[i1] == 5)
-                    {
-                        const int bigScore = 50;
-                        return bigScore;
-                    }
-                }
+        return 0;
+    }
 
-                return 0;
+    private static int AppleSauce4(int die1, int die2, int die3, int die4, int die5)
+    {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+        foreach (var die in dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                const int bigScore = 50;
+                return bigScore;
             }
         }
 
