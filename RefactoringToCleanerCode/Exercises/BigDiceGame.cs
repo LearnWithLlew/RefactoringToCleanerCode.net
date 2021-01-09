10e0dea6292ea933f6039f60eaee66e93279b4fd
Jay Bazuzi <jay@bazuzi.com>
2021-01-09 23:51:05 +0000

    r - remove block

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,13 +2,12 @@
 {
     public static int GetScore(ScoringType scoringType, int die1, int die2, int die3, int die4, int die5)
     {
+        foreach (INonSenseApple appleSauce in new INonSenseApple[]
+            {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()})
         {
-            foreach (INonSenseApple appleSauce in new INonSenseApple[]{ new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()})
+            if (appleSauce.Matches(scoringType))
             {
-                if (appleSauce.Matches(scoringType))
-                {
-                    return appleSauce.GetScore(die1, die2, die3, die4, die5);
-                }
+                return appleSauce.GetScore(die1, die2, die3, die4, die5);
             }
         }
 
