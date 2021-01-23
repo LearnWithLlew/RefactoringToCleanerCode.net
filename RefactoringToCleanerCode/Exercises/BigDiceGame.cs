d815edfc505e630b5dcfcf5dbeede3f8eb4bc97f
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 17:04:20 +0000

    r - convert to LINQ method chain

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -5,7 +5,9 @@ public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        return (from calculator in GetCalculators() where calculator.ShouldCalculate(scoringType) select calculator.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive)).FirstOrDefault();
+        return (GetCalculators()
+            .Where(calculator => calculator.ShouldCalculate(scoringType))
+            .Select(calculator => calculator.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive))).FirstOrDefault();
     }
 
     private static ICalculator[] GetCalculators()
