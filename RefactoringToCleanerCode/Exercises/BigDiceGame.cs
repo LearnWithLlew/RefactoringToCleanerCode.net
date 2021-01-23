374fe2edd47d0971965b9cd7c44ff3b205c94b99
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 16:30:45 +0000

    r - extract condition

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,7 +2,7 @@
 {
     public static int CalculateScore(ScoringType st, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        if (st == ScoringType.Pair)
+        if (Nonsense(st))
         {
             return CalculatePairScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
         }
@@ -22,5 +22,10 @@
         }
 
         return 0;
+    }
+
+    private static bool Nonsense(ScoringType st)
+    {
+        return st == ScoringType.Pair;
     }
 }
