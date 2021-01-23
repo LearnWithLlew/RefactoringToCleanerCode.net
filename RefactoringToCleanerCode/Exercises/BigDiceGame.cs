fc38268884df79e0c2d73fe225b51c9fa1e96a33
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 17:03:18 +0000

    R!! - many of four

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -5,7 +5,7 @@ public static class BigDiceGame
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
         {
-            foreach (ICalculator calculator in new ICalculator[]{ new CalculatePairScore(), new CalculateTwoPairScore(), new CalculateFullHouseScore() })
+            foreach (ICalculator calculator in new ICalculator[]{ new CalculatePairScore(), new CalculateTwoPairScore(), new CalculateFullHouseScore() , new CalculateFiveOfAKindScore() })
             {
                 if (calculator.ShouldCalculate(scoringType))
                 {
@@ -17,13 +17,7 @@ public static class BigDiceGame
         
 
         
-        {
-            var calculator = new CalculateFiveOfAKindScore();
-            if (calculator.ShouldCalculate(scoringType))
-            {
-                return calculator.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-        }
+        
 
         return 0;
     }
