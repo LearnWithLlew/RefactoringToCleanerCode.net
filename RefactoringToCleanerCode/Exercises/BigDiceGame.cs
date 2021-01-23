e680147ec3223b944294260cf1ac8d7db1d665ab
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 15:57:05 +0000

    r - extract Nonsense4 method.

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -21,23 +21,28 @@
 
             case ScoringType.FiveOfAKind:
             {
-                int[] dice1 = {dieOne, dieTwo, dieThree, dieFour, dieFive};
-                var counts2 = new int[6];
-                foreach (var die in dice1)
-                {
-                    counts2[die - 1]++;
-                }
+                return Nonsense4(dieOne, dieTwo, dieThree, dieFour, dieFive);
+            }
+        }
 
-                for (var i1 = 0; i1 != 6; i1++)
-                {
-                    if (counts2[i1] == 5)
-                    {
-                        const int bigScore = 50;
-                        return bigScore;
-                    }
-                }
+        return 0;
+    }
 
-                return 0;
+    private static int Nonsense4(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
+    {
+        int[] dice1 = {dieOne, dieTwo, dieThree, dieFour, dieFive};
+        var counts2 = new int[6];
+        foreach (var die in dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                const int bigScore = 50;
+                return bigScore;
             }
         }
 
