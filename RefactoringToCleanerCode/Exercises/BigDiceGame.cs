bb67aa7123f26dbc5b6f1871a9d69d395b0e5e3d
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 15:45:53 +0000

    r - extract Nonsense3 method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -17,45 +17,7 @@
 
             case ScoringType.FullHouse:
             {
-                int[] tallies;
-                var _2 = false;
-                int i;
-                var _2_at = 0;
-                var _3 = false;
-                var _3_at = 0;
-
-
-                tallies = new int[6];
-                tallies[dieOne - 1] += 1;
-                tallies[dieTwo - 1] += 1;
-                tallies[dieThree - 1] += 1;
-                tallies[dieFour - 1] += 1;
-                tallies[dieFive - 1] += 1;
-
-                for (i = 0; i != 6; i += 1)
-                {
-                    if (tallies[i] == 2)
-                    {
-                        _2 = true;
-                        _2_at = i + 1;
-                    }
-                }
-
-                for (i = 0; i != 6; i += 1)
-                {
-                    if (tallies[i] == 3)
-                    {
-                        _3 = true;
-                        _3_at = i + 1;
-                    }
-                }
-
-                if (_2 && _3)
-                {
-                    return _2_at * 2 + _3_at * 3;
-                }
-
-                return 0;
+                return Nonsense3(dieOne, dieTwo, dieThree, dieFour, dieFive);
             }
 
             case ScoringType.FiveOfAKind:
@@ -77,6 +39,49 @@
 
                 return 0;
             }
+        }
+
+        return 0;
+    }
+
+    private static int Nonsense3(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
+    {
+        int[] tallies;
+        var _2 = false;
+        int i;
+        var _2_at = 0;
+        var _3 = false;
+        var _3_at = 0;
+
+
+        tallies = new int[6];
+        tallies[dieOne - 1] += 1;
+        tallies[dieTwo - 1] += 1;
+        tallies[dieThree - 1] += 1;
+        tallies[dieFour - 1] += 1;
+        tallies[dieFive - 1] += 1;
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 2)
+            {
+                _2 = true;
+                _2_at = i + 1;
+            }
+        }
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 3)
+            {
+                _3 = true;
+                _3_at = i + 1;
+            }
+        }
+
+        if (_2 && _3)
+        {
+            return _2_at * 2 + _3_at * 3;
         }
 
         return 0;
