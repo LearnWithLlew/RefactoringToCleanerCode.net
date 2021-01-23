fb01a3e3b407311f530785c5729f2a1938b0e4a8
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 16:27:36 +0000

    r - switch to if statement

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,27 +2,21 @@
 {
     public static int CalculateScore(ScoringType st, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        switch (st)
+        if (st == ScoringType.Pair)
         {
-            case ScoringType.Pair:
-            {
-                return CalculatePairScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-
-            case ScoringType.TwoPair:
-            {
-                return CalculateTwoPairScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-
-            case ScoringType.FullHouse:
-            {
-                return CalculateFullHouseScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-
-            case ScoringType.FiveOfAKind:
-            {
-                return CalculateFiveOfAKindScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
+            return CalculatePairScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
+        }
+        else if (st == ScoringType.TwoPair)
+        {
+            return CalculateTwoPairScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
+        }
+        else if (st == ScoringType.FullHouse)
+        {
+            return CalculateFullHouseScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
+        }
+        else if (st == ScoringType.FiveOfAKind)
+        {
+            return CalculateFiveOfAKindScore.Calculate(dieOne, dieTwo, dieThree, dieFour, dieFive);
         }
 
         return 0;
