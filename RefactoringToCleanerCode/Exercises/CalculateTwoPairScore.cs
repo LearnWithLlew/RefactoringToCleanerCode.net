1e96bb0da9db283c8b9f776d85b42721768ae498
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 16:20:12 +0000

    r - create new class

---
+++ RefactoringToCleanerCode/Exercises/CalculateTwoPairScore.cs
@@ -0,0 +1,29 @@
+static internal class CalculateTwoPairScore
+{
+    public static int Nonsense2(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
+    {
+        var counts1 = new int[6];
+        counts1[dieOne - 1]++;
+        counts1[dieTwo - 1]++;
+        counts1[dieThree - 1]++;
+        counts1[dieFour - 1]++;
+        counts1[dieFive - 1]++;
+        var n = 0;
+        var score = 0;
+        for (var i = 0; i < 6; i += 1)
+        {
+            if (counts1[6 - i - 1] >= 2)
+            {
+                n++;
+                score += 6 - i;
+            }
+        }
+
+        if (n == 2)
+        {
+            return score * 2;
+        }
+
+        return 0;
+    }
+}
