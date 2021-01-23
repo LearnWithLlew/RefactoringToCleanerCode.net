4a28a8bc5557784af4c034720679ea242dae2795
Administrator <Administrator@EC2AMAZ-CCG57MH>
2021-01-23 16:23:11 +0000

    r - move to a new class

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -21,28 +21,7 @@
 
             case ScoringType.FiveOfAKind:
             {
-                return Nonsense4(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-        }
-
-        return 0;
-    }
-
-    private static int Nonsense4(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
-    {
-        int[] dice1 = {dieOne, dieTwo, dieThree, dieFour, dieFive};
-        var counts2 = new int[6];
-        foreach (var die in dice1)
-        {
-            counts2[die - 1]++;
-        }
-
-        for (var i1 = 0; i1 != 6; i1++)
-        {
-            if (counts2[i1] == 5)
-            {
-                const int bigScore = 50;
-                return bigScore;
+                return CalculateFiveOfAKindScore.Nonsense4(dieOne, dieTwo, dieThree, dieFour, dieFive);
             }
         }
 
---
+++ RefactoringToCleanerCode/Exercises/CalculateFiveOfAKindScore.cs
@@ -0,0 +1,23 @@
+static internal class CalculateFiveOfAKindScore
+{
+    public static int Nonsense4(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
+    {
+        int[] dice1 = {dieOne, dieTwo, dieThree, dieFour, dieFive};
+        var counts2 = new int[6];
+        foreach (var die in dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                const int bigScore = 50;
+                return bigScore;
+            }
+        }
+
+        return 0;
+    }
+}
