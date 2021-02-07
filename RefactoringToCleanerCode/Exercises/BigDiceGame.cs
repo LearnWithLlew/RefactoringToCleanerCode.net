a2d9cef56e2c8006143d4051a828f16f870fd0b8
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 02:05:07 +0000

    r - convert to LINQ expression

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -1,19 +1,9 @@
-﻿public static class BigDiceGame
+﻿using System.Linq;
+
+public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        foreach (IScorer scorer in new IScorer[]
-        {
-            new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator(), new DiceFullHouseScoreCalculator(),
-            new DiceFiveOfAKindScoreCalculator()
-        })
-        {
-            if (scorer.IsScorable(scoringType))
-            {
-                return scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-        }
-
-        return 0;
+        return (from scorer in new IScorer[] {new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator(), new DiceFullHouseScoreCalculator(), new DiceFiveOfAKindScoreCalculator()} where scorer.IsScorable(scoringType) select scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive)).FirstOrDefault();
     }
 }
