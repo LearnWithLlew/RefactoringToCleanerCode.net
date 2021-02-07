73cee8fe4258b5833be5409435a069b2ee3505e9
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 02:04:43 +0000

    R - many of four

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,20 +3,12 @@
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
         {
-            foreach (IScorer scorer in new IScorer[]{ new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator() , new DiceFullHouseScoreCalculator() })
+            foreach (IScorer scorer in new IScorer[]{ new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator() , new DiceFullHouseScoreCalculator(), new DiceFiveOfAKindScoreCalculator() })
             {
                 if (scorer.IsScorable(scoringType))
                 {
                     return scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive);
                 }
-            }
-        }
-
-        {
-            IScorer scorer = new DiceFiveOfAKindScoreCalculator();
-            if (scorer.IsScorable(scoringType))
-            {
-                return scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive);
             }
         }
 
