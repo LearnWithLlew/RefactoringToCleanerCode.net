8a21a5fc234c1c1c54fabb63ef2cb6218a7f7ce5
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 00:58:58 +0000

    r - extract obviousNonsense method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -21,22 +21,27 @@
 
             case ScoringType.FiveOfAKind:
             {
-                int[] dice1 = {dieOne, dieTwo, dieThree, dieFour, dieFive};
-                var counts2 = new int[6];
-                foreach (var die in dice1)
-                {
-                    counts2[die - 1]++;
-                }
+                return ObviousNonsenseFour(dieOne, dieTwo, dieThree, dieFour, dieFive);
+            }
+        }
 
-                for (var i1 = 0; i1 != 6; i1++)
-                {
-                    if (counts2[i1] == 5)
-                    {
-                        return 50;
-                    }
-                }
+        return 0;
+    }
 
-                return 0;
+    private static int ObviousNonsenseFour(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
+    {
+        int[] dice1 = {dieOne, dieTwo, dieThree, dieFour, dieFive};
+        var counts2 = new int[6];
+        foreach (var die in dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                return 50;
             }
         }
 
