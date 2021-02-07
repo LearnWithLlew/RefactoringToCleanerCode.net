0c651db952aed84bb03f14a2bb2a26649823a6c6
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 01:18:48 +0000

    r - move  method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -16,7 +16,7 @@
 
             case ScoringType.FullHouse:
             {
-                return ObviousNonsenseThree(dieOne, dieTwo, dieThree, dieFour, dieFive);
+                return DiceFullHouseScoreCalculator.ObviousNonsenseThree(dieOne, dieTwo, dieThree, dieFour, dieFive);
             }
 
             case ScoringType.FiveOfAKind:
@@ -43,49 +43,6 @@
             {
                 return 50;
             }
-        }
-
-        return 0;
-    }
-
-    private static int ObviousNonsenseThree(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
-    {
-        int[] tallies;
-        var _2 = false;
-        int i;
-        var _2_at = 0;
-        var _3 = false;
-        var _3_at = 0;
-
-
-        tallies = new int[6];
-        tallies[dieOne - 1] += 1;
-        tallies[dieTwo - 1] += 1;
-        tallies[dieThree - 1] += 1;
-        tallies[dieFour - 1] += 1;
-        tallies[dieFive - 1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-        {
-            if (tallies[i] == 2)
-            {
-                _2 = true;
-                _2_at = i + 1;
-            }
-        }
-
-        for (i = 0; i != 6; i += 1)
-        {
-            if (tallies[i] == 3)
-            {
-                _3 = true;
-                _3_at = i + 1;
-            }
-        }
-
-        if (_2 && _3)
-        {
-            return _2_at * 2 + _3_at * 3;
         }
 
         return 0;
---
+++ RefactoringToCleanerCode/Exercises/DiceFullHouseScoreCalculator.cs
@@ -0,0 +1,45 @@
+static internal class DiceFullHouseScoreCalculator
+{
+    public static int ObviousNonsenseThree(int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
+    {
+        int[] tallies;
+        var _2 = false;
+        int i;
+        var _2_at = 0;
+        var _3 = false;
+        var _3_at = 0;
+
+
+        tallies = new int[6];
+        tallies[dieOne - 1] += 1;
+        tallies[dieTwo - 1] += 1;
+        tallies[dieThree - 1] += 1;
+        tallies[dieFour - 1] += 1;
+        tallies[dieFive - 1] += 1;
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 2)
+            {
+                _2 = true;
+                _2_at = i + 1;
+            }
+        }
+
+        for (i = 0; i != 6; i += 1)
+        {
+            if (tallies[i] == 3)
+            {
+                _3 = true;
+                _3_at = i + 1;
+            }
+        }
+
+        if (_2 && _3)
+        {
+            return _2_at * 2 + _3_at * 3;
+        }
+
+        return 0;
+    }
+}
