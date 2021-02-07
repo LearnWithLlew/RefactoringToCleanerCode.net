03fc48c7350d548768b75031fd52a66d8adac843
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 01:25:09 +0000

    r - replace switch to if/else

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,27 +2,21 @@
 {
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        switch (scoringType)
+        if (scoringType == ScoringType.Pair)
         {
-            case ScoringType.Pair:
-            {
-                return DicePairScoreCalculator.ObviousNonsense(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-
-            case ScoringType.TwoPair:
-            {
-                return DiceTwoPairScoreCalculator.ObviousNonsenseTwo(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-
-            case ScoringType.FullHouse:
-            {
-                return DiceFullHouseScoreCalculator.ObviousNonsenseThree(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
-
-            case ScoringType.FiveOfAKind:
-            {
-                return DiceFiveOfAKindScoreCalculator.ObviousNonsenseFour(dieOne, dieTwo, dieThree, dieFour, dieFive);
-            }
+            return DicePairScoreCalculator.ObviousNonsense(dieOne, dieTwo, dieThree, dieFour, dieFive);
+        }
+        else if (scoringType == ScoringType.TwoPair)
+        {
+            return DiceTwoPairScoreCalculator.ObviousNonsenseTwo(dieOne, dieTwo, dieThree, dieFour, dieFive);
+        }
+        else if (scoringType == ScoringType.FullHouse)
+        {
+            return DiceFullHouseScoreCalculator.ObviousNonsenseThree(dieOne, dieTwo, dieThree, dieFour, dieFive);
+        }
+        else if (scoringType == ScoringType.FiveOfAKind)
+        {
+            return DiceFiveOfAKindScoreCalculator.ObviousNonsenseFour(dieOne, dieTwo, dieThree, dieFour, dieFive);
         }
 
         return 0;
