62caa94b23fe8fb253075c38a93c42a7634f3953
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 02:05:39 +0000

    r - extract method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -4,11 +4,16 @@ public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        return (new IScorer[]
-            {
-                new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator(), new DiceFullHouseScoreCalculator(),
-                new DiceFiveOfAKindScoreCalculator()
-            }.Where(scorer => scorer.IsScorable(scoringType))
+        return (GetScoringTypes().Where(scorer => scorer.IsScorable(scoringType))
             .Select(scorer => scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive))).FirstOrDefault();
+    }
+
+    private static IScorer[] GetScoringTypes()
+    {
+        return new IScorer[]
+        {
+            new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator(), new DiceFullHouseScoreCalculator(),
+            new DiceFiveOfAKindScoreCalculator()
+        };
     }
 }
