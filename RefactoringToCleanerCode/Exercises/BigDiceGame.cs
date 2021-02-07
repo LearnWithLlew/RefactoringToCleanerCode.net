e95dd4b84d26912ff9b8516b8ed39fb3570a457b
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-07 02:05:21 +0000

    r - convert to LINQ method chain

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -4,6 +4,11 @@ public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType scoringType, int dieOne, int dieTwo, int dieThree, int dieFour, int dieFive)
     {
-        return (from scorer in new IScorer[] {new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator(), new DiceFullHouseScoreCalculator(), new DiceFiveOfAKindScoreCalculator()} where scorer.IsScorable(scoringType) select scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive)).FirstOrDefault();
+        return (new IScorer[]
+            {
+                new DicePairScoreCalculator(), new DiceTwoPairScoreCalculator(), new DiceFullHouseScoreCalculator(),
+                new DiceFiveOfAKindScoreCalculator()
+            }.Where(scorer => scorer.IsScorable(scoringType))
+            .Select(scorer => scorer.CalculateScore(dieOne, dieTwo, dieThree, dieFour, dieFive))).FirstOrDefault();
     }
 }
