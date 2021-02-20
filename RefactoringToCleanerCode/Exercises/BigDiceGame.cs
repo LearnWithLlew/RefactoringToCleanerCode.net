c5010b72cba56d3c4f55466aaaeaae215841a2e3
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-20 19:10:51 +0000

    r - convert to LINQ expression

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -1,18 +1,11 @@
-﻿public static class BigDiceGame
+﻿using System.Linq;
+
+public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         var scorers = new IScorer[] {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()};
 
-        foreach (var scorer in scorers)
-        {
-            if (scorer.IsScorable(st))
-            {
-                return scorer.CalculateScore(die1, die2, die3, die4, die5);
-            }
-        }
-
-
-        return 0;
+        return (from scorer in scorers where scorer.IsScorable(st) select scorer.CalculateScore(die1, die2, die3, die4, die5)).FirstOrDefault();
     }
 }
