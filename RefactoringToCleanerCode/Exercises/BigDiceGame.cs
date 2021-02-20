a33f017dd7fa4867228fa13233d8b7aa08103b0a
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-20 19:11:53 +0000

    formatting

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -4,9 +4,17 @@ public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        var scorers = new IScorer[] {new Pair(), new TwoPair(), new FullHouse(), new FiveOfAKind()};
+        var scorers = new IScorer[]
+        {
+            new Pair(),
+            new TwoPair(),
+            new FullHouse(),
+            new FiveOfAKind()
+        };
 
-        return scorers.Where(scorer => scorer.IsScorable(st))
-            .Select(scorer => scorer.CalculateScore(die1, die2, die3, die4, die5)).FirstOrDefault();
+        return scorers
+            .Where(scorer => scorer.IsScorable(st))
+            .Select(scorer => scorer.CalculateScore(die1, die2, die3, die4, die5))
+            .FirstOrDefault();
     }
 }
