36b9b4ba4ca133c957f9916053285d57ed500f8d
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-20 18:59:30 +0000

    r - surround with block

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,9 +2,11 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (new Pair().IsScorable(st))
         {
-            return new Pair().CalculateScore(die1, die2, die3, die4, die5);
+            if (new Pair().IsScorable(st))
+            {
+                return new Pair().CalculateScore(die1, die2, die3, die4, die5);
+            }
         }
 
         if (new TwoPair().IsScorable(st))
