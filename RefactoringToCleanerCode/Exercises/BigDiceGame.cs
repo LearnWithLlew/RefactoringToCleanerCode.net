6b67de51c2655502362a786f19ac93bd01eae8f4
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-02-20 18:24:19 +0000

    r - move stuff to a pair class

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -6,7 +6,7 @@
         {
             case ScoringType.Pair:
             {
-                return Stuff(die1, die2, die3, die4, die5);
+                return Pair.Stuff(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.TwoPair:
@@ -113,27 +113,6 @@
         if (n == 2)
         {
             return score * 2;
-        }
-
-        return 0;
-    }
-
-    private static int Stuff(int die1, int die2, int die3, int die4, int die5)
-    {
-        const int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[die1 - 1]++;
-        tallies1[die2 - 1]++;
-        tallies1[die3 - 1]++;
-        tallies1[die4 - 1]++;
-        tallies1[die5 - 1]++;
-        for (var i = 6 - 1; i >= 0; i--)
-        {
-            if (tallies1[i] >= value)
-            {
-                return (i + 1) * value;
-            }
         }
 
         return 0;
---
+++ RefactoringToCleanerCode/Exercises/Pair.cs
@@ -0,0 +1,23 @@
+static internal class Pair
+{
+    public static int Stuff(int die1, int die2, int die3, int die4, int die5)
+    {
+        const int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[die1 - 1]++;
+        tallies1[die2 - 1]++;
+        tallies1[die3 - 1]++;
+        tallies1[die4 - 1]++;
+        tallies1[die5 - 1]++;
+        for (var i = 6 - 1; i >= 0; i--)
+        {
+            if (tallies1[i] >= value)
+            {
+                return (i + 1) * value;
+            }
+        }
+
+        return 0;
+    }
+}
