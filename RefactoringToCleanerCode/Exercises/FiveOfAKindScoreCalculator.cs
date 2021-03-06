029c23a938700b80d864b5782082fdd9ef619c59
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 23:18:23 +0000

    r - extract interface

--- RefactoringToCleanerCode/Exercises/FiveOfAKindScoreCalculator.cs
+++ RefactoringToCleanerCode/Exercises/FiveOfAKindScoreCalculator.cs
@@ -1,4 +1,4 @@
-internal class FiveOfAKindScoreCalculator
+internal class FiveOfAKindScoreCalculator : IScoreCalculator
 {
     public int CalculateScore(int die1, int die2, int die3, int die4, int die5)
     {
