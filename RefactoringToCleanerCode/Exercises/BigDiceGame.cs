766082c6bdd213738caca3e9d4db263e84092edf
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 23:22:51 +0000

    R!! - many of two

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,15 +3,7 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            foreach(IScoreCalculator scoreCalculator in new IScoreCalculator[] {new PairScoreCalculator()})
-            if (scoreCalculator.IsApplicable(st))
-            {
-                return scoreCalculator.CalculateScore(die1, die2, die3, die4, die5);
-            }
-        }
-
-        {
-            IScoreCalculator scoreCalculator = new TwoPairScoreCalculator();
+            foreach(IScoreCalculator scoreCalculator in new IScoreCalculator[] {new PairScoreCalculator(), new TwoPairScoreCalculator() })
             if (scoreCalculator.IsApplicable(st))
             {
                 return scoreCalculator.CalculateScore(die1, die2, die3, die4, die5);
