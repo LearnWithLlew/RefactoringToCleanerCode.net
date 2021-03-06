99f77814dda6d3059f609b2d64f3cd84106f3709
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 22:38:15 +0000

    r - change switch to if-else

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,27 +2,21 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        switch (st)
+        if (st == ScoringType.Pair)
         {
-            case ScoringType.Pair:
-            {
-                return PairScoreCalculator.FirstPararaph(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.TwoPair:
-            {
-                return TwoPairScoreCalculator.SecondParagraph(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.FullHouse:
-            {
-                return FullHouseScoreCalculator.ThirdParagraph(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.FiveOfAKind:
-            {
-                return FiveOfAKindScoreCalculator.FourthParagraph(die1, die2, die3, die4, die5);
-            }
+            return PairScoreCalculator.FirstPararaph(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.TwoPair)
+        {
+            return TwoPairScoreCalculator.SecondParagraph(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.FullHouse)
+        {
+            return FullHouseScoreCalculator.ThirdParagraph(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.FiveOfAKind)
+        {
+            return FiveOfAKindScoreCalculator.FourthParagraph(die1, die2, die3, die4, die5);
         }
 
         return 0;
