2c33a77e30c395dfce323a5e0851f8796dfcfb28
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 23:25:39 +0000

    r - inline variable

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -4,9 +4,7 @@ public static class BigDiceGame
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        var scoreCalculators = GetScoreCalculators();
-
-        return scoreCalculators.Where(scoreCalculator => scoreCalculator.IsApplicable(st))
+        return GetScoreCalculators().Where(scoreCalculator => scoreCalculator.IsApplicable(st))
             .Select(scoreCalculator => scoreCalculator.CalculateScore(die1, die2, die3, die4, die5)).FirstOrDefault();
     }
 
