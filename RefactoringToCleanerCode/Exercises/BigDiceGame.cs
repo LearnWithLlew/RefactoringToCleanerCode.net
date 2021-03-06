34d364e25808f08e131655b37e10394a8ed12f79
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 23:16:54 +0000

    r - specifiy type explicitly

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,7 +3,7 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            var scoreCalculator = new PairScoreCalculator();
+            PairScoreCalculator scoreCalculator = new PairScoreCalculator();
             if (scoreCalculator.IsApplicable(st))
             {
                 return scoreCalculator.CalculateScore(die1, die2, die3, die4, die5);
@@ -11,7 +11,7 @@
         }
 
         {
-            var scoreCalculator = new TwoPairScoreCalculator();
+            TwoPairScoreCalculator scoreCalculator = new TwoPairScoreCalculator();
             if (scoreCalculator.IsApplicable(st))
             {
                 return scoreCalculator.CalculateScore(die1, die2, die3, die4, die5);
@@ -19,7 +19,7 @@
         }
 
         {
-            var scoreCalculator = new FullHouseScoreCalculator();
+            FullHouseScoreCalculator scoreCalculator = new FullHouseScoreCalculator();
             if (scoreCalculator.IsApplicable(st))
             {
                 return scoreCalculator.CalculateScore(die1, die2, die3, die4, die5);
@@ -27,7 +27,7 @@
         }
 
         {
-            var scoreCalculator = new FiveOfAKindScoreCalculator();
+            FiveOfAKindScoreCalculator scoreCalculator = new FiveOfAKindScoreCalculator();
             if (scoreCalculator.IsApplicable(st))
             {
                 return new FiveOfAKindScoreCalculator().CalculateScore(die1, die2, die3, die4, die5);
