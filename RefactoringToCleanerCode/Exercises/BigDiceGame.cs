929536aa6cc5a5bc71cb7e9129dcd180f15ded15
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 22:36:45 +0000

    r - Extract FourthParagraph into FiveOfAKindScoreCalculator class method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -21,28 +21,7 @@
 
             case ScoringType.FiveOfAKind:
             {
-                return FourthParagraph(die1, die2, die3, die4, die5);
-            }
-        }
-
-        return 0;
-    }
-
-    private static int FourthParagraph(int die1, int die2, int die3, int die4, int die5)
-    {
-        int[] dice1 = {die1, die2, die3, die4, die5};
-        var counts2 = new int[6];
-        foreach (var die in dice1)
-        {
-            counts2[die - 1]++;
-        }
-
-        for (var i1 = 0; i1 != 6; i1++)
-        {
-            if (counts2[i1] == 5)
-            {
-                const int bigScore = 50;
-                return bigScore;
+                return FiveOfAKindScoreCalculator.FourthParagraph(die1, die2, die3, die4, die5);
             }
         }
 
