3aae118816d5cf864a2bef3dc7abe911e2736d07
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 23:15:38 +0000

    r - extract variable

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,9 +3,10 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            if (new PairScoreCalculator().IsApplicable(st))
+            var scoreCalculator = new PairScoreCalculator();
+            if (scoreCalculator.IsApplicable(st))
             {
-                return new PairScoreCalculator().CalculateScore(die1, die2, die3, die4, die5);
+                return scoreCalculator.CalculateScore(die1, die2, die3, die4, die5);
             }
         }
 
