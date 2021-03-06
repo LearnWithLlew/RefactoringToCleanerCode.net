c1aeb84b90b2501d40701a0d8f37ff9bd8f36902
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-06 23:17:53 +0000

    r - extract interface

--- RefactoringToCleanerCode/Exercises/TwoPairScoreCalculator.cs
+++ RefactoringToCleanerCode/Exercises/TwoPairScoreCalculator.cs
@@ -1,4 +1,4 @@
-internal class TwoPairScoreCalculator
+internal class TwoPairScoreCalculator :IScoreCalculator
 {
     public int CalculateScore(int die1, int die2, int die3, int die4, int die5)
     {
