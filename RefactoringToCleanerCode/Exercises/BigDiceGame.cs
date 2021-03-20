8f55119361664e9b6f2927c97149d3c30e648e5a
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 16:23:13 +0000

    R - many of two

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -5,7 +5,7 @@ public static class BigDiceGame
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            foreach (IScore score in new IScore[]{ new PairScore() })
+            foreach (IScore score in new IScore[]{ new PairScore(), new TwoPairScore() })
             {
                 if (score.IsSomeCondition(st))
                 {
@@ -15,13 +15,7 @@ public static class BigDiceGame
             }
         }
 
-        {
-            IScore score = new TwoPairScore();
-            if (score.IsSomeCondition(st))
-            {
-                return score.Evaluate(die1, die2, die3, die4, die5);
-            }
-        }
+
         {
             IScore score = new FullHouseScore();
             if (score.IsSomeCondition(st))
