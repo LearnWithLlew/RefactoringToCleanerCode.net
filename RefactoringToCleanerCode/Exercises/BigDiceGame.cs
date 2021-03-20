b74b06898f89944c86b39f64446cd1358b237645
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 16:21:17 +0000

    R - many of one

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -1,12 +1,17 @@
-﻿public static class BigDiceGame 
+﻿using System.Linq.Expressions;
+
+public static class BigDiceGame 
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            IScore score = new PairScore();
-            if (score.IsSomeCondition(st))
+            foreach (IScore score in new IScore[]{ new PairScore() })
             {
-                return score.Evaluate(die1, die2, die3, die4, die5);
+                if (score.IsSomeCondition(st))
+                {
+                    return score.Evaluate(die1, die2, die3, die4, die5);
+                }
+                
             }
         }
 
