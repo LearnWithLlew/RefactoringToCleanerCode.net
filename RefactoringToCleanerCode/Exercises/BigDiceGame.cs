0527085243669a5a1244d7d6caec082d6347c1f2
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 14:57:00 +0000

    r - extract

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -16,7 +16,7 @@
 
             case ScoringType.FullHouse:
             {
-                return SomeFunction2(die1, die2, die3, die4, die5);
+                return FullHouseScore.SomeFunction2(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.FiveOfAKind:
@@ -71,49 +71,6 @@
         if (n == 2)
         {
             return score * 2;
-        }
-
-        return 0;
-    }
-
-    private static int SomeFunction2(int die1, int die2, int die3, int die4, int die5)
-    {
-        int[] tallies;
-        var _2 = false;
-        int i;
-        var _2_at = 0;
-        var _3 = false;
-        var _3_at = 0;
-
-
-        tallies = new int[6];
-        tallies[die1 - 1] += 1;
-        tallies[die2 - 1] += 1;
-        tallies[die3 - 1] += 1;
-        tallies[die4 - 1] += 1;
-        tallies[die5 - 1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-        {
-            if (tallies[i] == 2)
-            {
-                _2 = true;
-                _2_at = i + 1;
-            }
-        }
-
-        for (i = 0; i != 6; i += 1)
-        {
-            if (tallies[i] == 3)
-            {
-                _3 = true;
-                _3_at = i + 1;
-            }
-        }
-
-        if (_2 && _3)
-        {
-            return _2_at * 2 + _3_at * 3;
         }
 
         return 0;
