ecd25b3b50b77b3a081f0668c069fc29f7f47234
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 15:27:35 +0000

    r - surround

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,9 +2,11 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (PairScore.IsSomeCondition(st))
         {
-            return PairScore.Evaluate(die1, die2, die3, die4, die5);
+            if (PairScore.IsSomeCondition(st))
+            {
+                return PairScore.Evaluate(die1, die2, die3, die4, die5);
+            }
         }
 
         if (TwoPairScore.IsSomeCondition(st))
