6fa9de592dc4b61f1f23d2b733ae5fe3d3c61c23
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 14:43:24 +0000

    r - extract

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -7,23 +7,7 @@
         {
             case ScoringType.Pair:
             {
-                const int value = 2;
-                int[] tallies1;
-                tallies1 = new int[6];
-                tallies1[die1 - 1]++;
-                tallies1[die2 - 1]++;
-                tallies1[die3 - 1]++;
-                tallies1[die4 - 1]++;
-                tallies1[die5 - 1]++;
-                for (var i = 6 - 1; i >= 0; i--)
-                {
-                    if (tallies1[i] >= value)
-                    {
-                        return (i + 1) * value;
-                    }
-                }
-
-                return 0;
+                return SomeFunction4(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.TwoPair:
@@ -39,6 +23,27 @@
             case ScoringType.FiveOfAKind:
             {
                 return SomeFunction(die1, die2, die3, die4, die5, bigScore);
+            }
+        }
+
+        return 0;
+    }
+
+    private static int SomeFunction4(int die1, int die2, int die3, int die4, int die5)
+    {
+        const int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[die1 - 1]++;
+        tallies1[die2 - 1]++;
+        tallies1[die3 - 1]++;
+        tallies1[die4 - 1]++;
+        tallies1[die5 - 1]++;
+        for (var i = 6 - 1; i >= 0; i--)
+        {
+            if (tallies1[i] >= value)
+            {
+                return (i + 1) * value;
             }
         }
 
