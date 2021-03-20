a53aa82777694ba3f5c7700cf4555cc210792643
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 16:11:18 +0000

    r - show type

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,7 +3,7 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            var score = new PairScore();
+            PairScore score = new PairScore();
             if (score.IsSomeCondition(st))
             {
                 return score.Evaluate(die1, die2, die3, die4, die5);
@@ -11,21 +11,21 @@
         }
 
         {
-            var score = new TwoPairScore();
+            TwoPairScore score = new TwoPairScore();
             if (score.IsSomeCondition(st))
             {
                 return score.Evaluate(die1, die2, die3, die4, die5);
             }
         }
         {
-            var score = new FullHouseScore();
+            FullHouseScore score = new FullHouseScore();
             if (score.IsSomeCondition(st))
             {
                 return score.Evaluate(die1, die2, die3, die4, die5);
             }
         }
         {
-            var score = new FiveOfKindScore();
+            FiveOfKindScore score = new FiveOfKindScore();
             if (score.IsSomeCondition(st))
             {
                 return score.Evaluate(die1, die2, die3, die4, die5);
