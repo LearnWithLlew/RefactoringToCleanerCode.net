a591245dbd573d875536bd821e432eeac25eddc7
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 16:12:32 +0000

    r - use iscore

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,7 +3,7 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            PairScore score = new PairScore();
+            IScore score = new PairScore();
             if (score.IsSomeCondition(st))
             {
                 return score.Evaluate(die1, die2, die3, die4, die5);
