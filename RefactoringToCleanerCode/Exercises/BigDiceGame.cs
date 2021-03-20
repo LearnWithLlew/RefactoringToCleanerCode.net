3bcebb0ca3e97331cb30faddb41608cb16776d2e
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 14:47:55 +0000

    r - extract

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -21,22 +21,27 @@
 
             case ScoringType.FiveOfAKind:
             {
-                int[] dice1 = {die1, die2, die3, die4, die5};
-                var counts2 = new int[6];
-                foreach (var die in dice1)
-                {
-                    counts2[die - 1]++;
-                }
+                return SomeFunction(die1, die2, die3, die4, die5);
+            }
+        }
 
-                for (var i1 = 0; i1 != 6; i1++)
-                {
-                    if (counts2[i1] == 5)
-                    {
-                        return 50;
-                    }
-                }
+        return 0;
+    }
 
-                return 0;
+    private static int SomeFunction(int die1, int die2, int die3, int die4, int die5)
+    {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+        foreach (var die in dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                return 50;
             }
         }
 
