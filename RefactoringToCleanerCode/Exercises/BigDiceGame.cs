ca47bb4989188b5bf407c4b70edcdeda2a5181d4
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 15:56:40 +0000

    r - extract local varible

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,9 +3,10 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            if (new PairScore().IsSomeCondition(st))
+            var score = new PairScore();
+            if (score.IsSomeCondition(st))
             {
-                return new PairScore().Evaluate(die1, die2, die3, die4, die5);
+                return score.Evaluate(die1, die2, die3, die4, die5);
             }
         }
 
