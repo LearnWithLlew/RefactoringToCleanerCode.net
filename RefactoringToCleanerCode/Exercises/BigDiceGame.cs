239dffab3b1a7f6b02f16c3b4c042af861ff92ae
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 15:11:57 +0000

    r - convert to if-else

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,27 +2,21 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        switch (st)
+        if (st == ScoringType.Pair)
         {
-            case ScoringType.Pair:
-            {
-                return PairScore.Evaluate(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.TwoPair:
-            {
-                return TwoPairScore.Evaluate(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.FullHouse:
-            {
-                return FullHouseScore.Evaluate(die1, die2, die3, die4, die5);
-            }
-
-            case ScoringType.FiveOfAKind:
-            {
-                return FiveOfKindScore.Evaluate(die1, die2, die3, die4, die5);
-            }
+            return PairScore.Evaluate(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.TwoPair)
+        {
+            return TwoPairScore.Evaluate(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.FullHouse)
+        {
+            return FullHouseScore.Evaluate(die1, die2, die3, die4, die5);
+        }
+        else if (st == ScoringType.FiveOfAKind)
+        {
+            return FiveOfKindScore.Evaluate(die1, die2, die3, die4, die5);
         }
 
         return 0;
