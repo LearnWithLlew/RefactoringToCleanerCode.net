5e8643a28db3f73720f0fbe778f5a1bcce317226
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-03-20 14:59:42 +0000

    r - extract

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -6,7 +6,7 @@
         {
             case ScoringType.Pair:
             {
-                return SomeFunction4(die1, die2, die3, die4, die5);
+                return PairScore.SomeFunction4(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.TwoPair:
@@ -22,27 +22,6 @@
             case ScoringType.FiveOfAKind:
             {
                 return FiveOfKindScore.Evaluate(die1, die2, die3, die4, die5);
-            }
-        }
-
-        return 0;
-    }
-
-    private static int SomeFunction4(int die1, int die2, int die3, int die4, int die5)
-    {
-        const int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[die1 - 1]++;
-        tallies1[die2 - 1]++;
-        tallies1[die3 - 1]++;
-        tallies1[die4 - 1]++;
-        tallies1[die5 - 1]++;
-        for (var i = 6 - 1; i >= 0; i--)
-        {
-            if (tallies1[i] >= value)
-            {
-                return (i + 1) * value;
             }
         }
 
