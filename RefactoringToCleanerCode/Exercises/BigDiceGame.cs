5adc3af7ff209af4ada21316fc681392f28e31ab
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-05-31 14:26:06 +0000

    r - move FirstExtract to PairFinder class

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -6,7 +6,7 @@
         {
             case ScoringType.Pair:
             {
-                return FirstExtract(die1, die2, die3, die4, die5);
+                return PairFinder.FirstExtract(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.TwoPair:
@@ -114,27 +114,6 @@
         if (n == 2)
         {
             return score * 2;
-        }
-
-        return 0;
-    }
-
-    private static int FirstExtract(int die1, int die2, int d3, int dieFour, int fifthDie)
-    {
-        const int value = 2;
-        int[] tallies1;
-        tallies1 = new int[6];
-        tallies1[die1 - 1]++;
-        tallies1[die2 - 1]++;
-        tallies1[d3 - 1]++;
-        tallies1[dieFour - 1]++;
-        tallies1[fifthDie - 1]++;
-        for (var i = 6 - 1; i >= 0; i--)
-        {
-            if (tallies1[i] >= value)
-            {
-                return (i + 1) * value;
-            }
         }
 
         return 0;
