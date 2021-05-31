05b5288f20121ef7f7d091de144419d0b60a32de
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-05-31 14:40:39 +0000

    r - Moved FourthExtract to FiveOfAKindFinder class

---
+++ RefactoringToCleanerCode/Exercises/FiveOfAKindFinder.cs
@@ -0,0 +1,23 @@
+static internal class FiveOfAKindFinder
+{
+    public static int FourthExtract(int die1, int die2, int die3, int die4, int die5)
+    {
+        int[] dice1 = {die1, die2, die3, die4, die5};
+        var counts2 = new int[6];
+        foreach (var die in dice1)
+        {
+            counts2[die - 1]++;
+        }
+
+        for (var i1 = 0; i1 != 6; i1++)
+        {
+            if (counts2[i1] == 5)
+            {
+                const int bigScore = 50;
+                return bigScore;
+            }
+        }
+
+        return 0;
+    }
+}
