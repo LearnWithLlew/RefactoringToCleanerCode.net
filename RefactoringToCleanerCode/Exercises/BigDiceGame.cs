a29a241dce88b02f11eea47523165e6da10d62fd
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-05-31 15:30:15 +0000

    r - remove else

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -6,15 +6,17 @@
         {
             return PairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
         }
-        else if (st == ScoringType.TwoPair)
+
+        if (st == ScoringType.TwoPair)
         {
             return TwoPairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
         }
-        else if (st == ScoringType.FullHouse)
+        if (st == ScoringType.FullHouse)
         {
             return FullHouseScoreCalculator.Calculate(die1, die2, die3, die4, die5);
         }
-        else if (st == ScoringType.FiveOfAKind)
+
+        if (st == ScoringType.FiveOfAKind)
         {
             return FiveOfAKindScoreCalculator.Calculate(die1, die2, die3, die4, die5);
         }
