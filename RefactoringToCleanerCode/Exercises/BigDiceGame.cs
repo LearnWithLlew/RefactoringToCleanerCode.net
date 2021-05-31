0600915f2040d1e39eba2975fd03f147b3cb6bb8
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-05-31 14:27:48 +0000

    r - move SecondExtract method to TowPairFinder class

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -11,7 +11,7 @@
 
             case ScoringType.TwoPair:
             {
-                return SecondExtract(die1, die2, die3, die4, die5);
+                return TwoPairFinder.SecondExtract(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.FullHouse:
@@ -87,33 +87,6 @@
         if (_2 && _3)
         {
             return _2_at * 2 + _3_at * 3;
-        }
-
-        return 0;
-    }
-
-    private static int SecondExtract(int die1, int die2, int d3, int dieFour, int fifthDie)
-    {
-        var counts1 = new int[6];
-        counts1[die1 - 1]++;
-        counts1[die2 - 1]++;
-        counts1[d3 - 1]++;
-        counts1[dieFour - 1]++;
-        counts1[fifthDie - 1]++;
-        var n = 0;
-        var score = 0;
-        for (var i = 0; i < 6; i += 1)
-        {
-            if (counts1[6 - i - 1] >= 2)
-            {
-                n++;
-                score += 6 - i;
-            }
-        }
-
-        if (n == 2)
-        {
-            return score * 2;
         }
 
         return 0;
---
+++ RefactoringToCleanerCode/Exercises/TwoPairFinder.cs
@@ -0,0 +1,29 @@
+static internal class TwoPairFinder
+{
+    public static int SecondExtract(int die1, int die2, int d3, int dieFour, int fifthDie)
+    {
+        var counts1 = new int[6];
+        counts1[die1 - 1]++;
+        counts1[die2 - 1]++;
+        counts1[d3 - 1]++;
+        counts1[dieFour - 1]++;
+        counts1[fifthDie - 1]++;
+        var n = 0;
+        var score = 0;
+        for (var i = 0; i < 6; i += 1)
+        {
+            if (counts1[6 - i - 1] >= 2)
+            {
+                n++;
+                score += 6 - i;
+            }
+        }
+
+        if (n == 2)
+        {
+            return score * 2;
+        }
+
+        return 0;
+    }
+}
