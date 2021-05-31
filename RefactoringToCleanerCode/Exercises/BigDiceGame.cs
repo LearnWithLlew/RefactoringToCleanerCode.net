9c5f533e6725db2824f0a6692cc078e6ba13ed86
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-05-31 15:31:35 +0000

    r - extract method

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,7 +2,7 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (st == ScoringType.Pair)
+        if (IsFirstCondition(st))
         {
             return PairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
         }
@@ -11,6 +11,7 @@
         {
             return TwoPairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
         }
+
         if (st == ScoringType.FullHouse)
         {
             return FullHouseScoreCalculator.Calculate(die1, die2, die3, die4, die5);
@@ -22,5 +23,10 @@
         }
 
         return 0;
+    }
+
+    private static bool IsFirstCondition(ScoringType st)
+    {
+        return st == ScoringType.Pair;
     }
 }
