92b49bd4ddd593d936986a72833fbd7473ed4907
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-01 13:18:22 +0000

    r - Sourounded first if with block.

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,11 +2,13 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
-        if (PairScoreCalculator.IsSupported(st))
         {
-            return PairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
+            if (PairScoreCalculator.IsSupported(st))
+            {
+                return PairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
+            }
         }
-
+        
         if (TwoPairScoreCalculator.IsSupported(st))
         {
             return TwoPairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
