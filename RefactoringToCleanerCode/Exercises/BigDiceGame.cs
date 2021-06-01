d629d5a173c8b36a8742e4dde65b0a90424c2f3d
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-01 13:18:52 +0000

    r - Sourounded second if with block.

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -8,10 +8,12 @@
                 return PairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
             }
         }
-        
-        if (TwoPairScoreCalculator.IsSupported(st))
+
         {
-            return TwoPairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
+            if (TwoPairScoreCalculator.IsSupported(st))
+            {
+                return TwoPairScoreCalculator.Calculate(die1, die2, die3, die4, die5);
+            }
         }
 
         if (FullHouseScoreCalculator.IsSupported(st))
