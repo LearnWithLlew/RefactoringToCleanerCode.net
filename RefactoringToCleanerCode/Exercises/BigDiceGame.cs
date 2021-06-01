5ddc7b27b1a60110f14f3214d1cd177a4404d2d1
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-01 14:03:53 +0000

    r - introduce local variable

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,9 +3,10 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            if (new PairScoreCalculator().IsSupported(st))
+            var calculator = new PairScoreCalculator();
+            if (calculator.IsSupported(st))
             {
-                return new PairScoreCalculator().Calculate(die1, die2, die3, die4, die5);
+                return calculator.Calculate(die1, die2, die3, die4, die5);
             }
         }
 
