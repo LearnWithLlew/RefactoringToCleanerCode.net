48a08d93731d9ae62622d6b312301a36981a4469
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-01 14:49:33 +0000

    r - remove the braces

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -2,14 +2,16 @@
 {
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
+        var calculators = new ICalculator[]
         {
-            var calculators = new ICalculator[]{new PairScoreCalculator(), new TwoPairScoreCalculator(), new FullHouseScoreCalculator(), new FiveOfAKindScoreCalculator()};
-            foreach(ICalculator calculator in calculators)
+            new PairScoreCalculator(), new TwoPairScoreCalculator(), new FullHouseScoreCalculator(),
+            new FiveOfAKindScoreCalculator()
+        };
+        foreach (ICalculator calculator in calculators)
+        {
+            if (calculator.IsSupported(st))
             {
-                if (calculator.IsSupported(st))
-                {
-                    return calculator.Calculate(die1, die2, die3, die4, die5);
-                }
+                return calculator.Calculate(die1, die2, die3, die4, die5);
             }
         }
 
