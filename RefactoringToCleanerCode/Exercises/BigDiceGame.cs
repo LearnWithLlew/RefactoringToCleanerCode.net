573e77b686f6c6892f09bb0de35653b5613046c9
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-01 14:37:28 +0000

    R - added FiveOfAKind

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -3,20 +3,12 @@
     public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         {
-            foreach(ICalculator calculator in new ICalculator[]{new PairScoreCalculator(), new TwoPairScoreCalculator(), new FullHouseScoreCalculator()})
+            foreach(ICalculator calculator in new ICalculator[]{new PairScoreCalculator(), new TwoPairScoreCalculator(), new FullHouseScoreCalculator(), new FiveOfAKindScoreCalculator()})
             {
                 if (calculator.IsSupported(st))
                 {
                     return calculator.Calculate(die1, die2, die3, die4, die5);
                 }
-            }
-        }
-
-        {
-            ICalculator calculator = new FiveOfAKindScoreCalculator();
-            if (calculator.IsSupported(st))
-            {
-                return calculator.Calculate(die1, die2, die3, die4, die5);
             }
         }
 
