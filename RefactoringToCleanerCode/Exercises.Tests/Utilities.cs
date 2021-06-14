7ddc0fd000359567c0613330e8d37e81b6a0902b
Jay Bazuzi <jay@bazuzi.com>
2021-06-13 17:52:35 -0700

    Update Utilities.cs

--- RefactoringToCleanerCode/Exercises.Tests/Utilities.cs
+++ RefactoringToCleanerCode/Exercises.Tests/Utilities.cs
@@ -4,11 +4,6 @@ using ApprovalTests.Combinations;
 
 internal static class Utilities
 {
-    public static IEnumerable<TEnum> GetEnumValues<TEnum>()
-    {
-        return (TEnum[]) Enum.GetValues(typeof(TEnum));
-    }
-
     public static void VerifyAllCombinations<T1, T2, T3, T4, T5, T6, TResult>(
         Func<T1, T2, T3, T4, T5, T6, TResult> processCall,
         IEnumerable<T1> aList, IEnumerable<T2> bList, IEnumerable<T3> cList, IEnumerable<T4> dList,
