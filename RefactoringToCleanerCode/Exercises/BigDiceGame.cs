cfd79df45808420750b58b565e3017a118c49565
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-25 20:43:34 +0000

    r - Extract AppleSauce

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -7,23 +7,7 @@
         {
             case ScoringType.Pair:
             {
-                const int value = 2;
-                int[] tallies1;
-                tallies1 = new int[6];
-                tallies1[die1 - 1]++;
-                tallies1[die2 - 1]++;
-                tallies1[d3 - 1]++;
-                tallies1[dieFour - 1]++;
-                tallies1[fifthDie - 1]++;
-                for (var i = 6 - 1; i >= 0; i--)
-                {
-                    if (tallies1[i] >= value)
-                    {
-                        return (i + 1) * value;
-                    }
-                }
-
-                return 0;
+                return AppleSauce(die1, die2, d3, dieFour, fifthDie);
             }
 
             case ScoringType.TwoPair:
@@ -114,6 +98,27 @@
                 }
 
                 return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int AppleSauce(int die1, int die2, int d3, int dieFour, int fifthDie)
+    {
+        const int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[die1 - 1]++;
+        tallies1[die2 - 1]++;
+        tallies1[d3 - 1]++;
+        tallies1[dieFour - 1]++;
+        tallies1[fifthDie - 1]++;
+        for (var i = 6 - 1; i >= 0; i--)
+        {
+            if (tallies1[i] >= value)
+            {
+                return (i + 1) * value;
             }
         }
 
