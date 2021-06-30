fbcbc297178adc996e51490b565d1ae630d7d955
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-30 20:41:47 +0000

    r - Move method to class

---
+++ RefactoringToCleanerCode/Exercises/ScoreAPairOfDice.cs
@@ -0,0 +1,23 @@
+static internal class ScoreAPairOfDice
+{
+    public static int AppleSauce(int die1, int die2, int d3, int dieFour, int fifthDie)
+    {
+        const int value = 2;
+        int[] tallies1;
+        tallies1 = new int[6];
+        tallies1[die1 - 1]++;
+        tallies1[die2 - 1]++;
+        tallies1[d3 - 1]++;
+        tallies1[dieFour - 1]++;
+        tallies1[fifthDie - 1]++;
+        for (var i = 6 - 1; i >= 0; i--)
+        {
+            if (tallies1[i] >= value)
+            {
+                return (i + 1) * value;
+            }
+        }
+
+        return 0;
+    }
+}
