ecbab0dfe6d52dfada74f07d1e843bb2a8f0c9f5
RefactoringPipelinesTeam <69706407+RefactoringPipelinesTeam@users.noreply.github.com>
2021-06-30 20:22:38 +0000

    r - rename parameter

--- RefactoringToCleanerCode/Exercises/BigDiceGame.cs
+++ RefactoringToCleanerCode/Exercises/BigDiceGame.cs
@@ -1,13 +1,13 @@
 ﻿public static class BigDiceGame
 {
-    public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int fifthDie)
+    public static int CalculateScore(ScoringType st, int die1, int die2, int die3, int die4, int die5)
     {
         const int bigScore = 50;
         switch (st)
         {
             case ScoringType.Pair:
             {
-                return AppleSauce(die1, die2, die3, die4, fifthDie);
+                return AppleSauce(die1, die2, die3, die4, die5);
             }
 
             case ScoringType.TwoPair:
@@ -17,7 +17,7 @@
                 counts1[die2 - 1]++;
                 counts1[die3 - 1]++;
                 counts1[die4 - 1]++;
-                counts1[fifthDie - 1]++;
+                counts1[die5 - 1]++;
                 var n = 0;
                 var score = 0;
                 for (var i = 0; i < 6; i += 1)
@@ -52,7 +52,7 @@
                 tallies[die2 - 1] += 1;
                 tallies[die3 - 1] += 1;
                 tallies[die4 - 1] += 1;
-                tallies[fifthDie - 1] += 1;
+                tallies[die5 - 1] += 1;
 
                 for (i = 0; i != 6; i += 1)
                 {
@@ -82,7 +82,7 @@
 
             case ScoringType.FiveOfAKind:
             {
-                int[] dice1 = {die1, die2, die3, die4, fifthDie};
+                int[] dice1 = {die1, die2, die3, die4, die5};
                 var counts2 = new int[6];
                 foreach (var die in dice1)
                 {
